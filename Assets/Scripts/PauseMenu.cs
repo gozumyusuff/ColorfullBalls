328bacf884f0b5a7122707c79d23a74da561de70
gozumyusuff <139639043+gozumyusuff@users.noreply.github.com>
2023-10-05 17:29:26 +0300

    pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
